579a89dc6952ca331e406b3386abbb81555318d7
koradiyayash26 <yashkoradiya26@gmail.com>
2024-06-12 12:41:58 +0530

    add active user first char on profile

--- src/components/layouts/header.jsx
+++ src/components/layouts/header.jsx
@@ -1,5 +1,5 @@
 import { MobileSidebar } from "./mobile-sidebar";
-import { CircleUser } from "lucide-react";
+import { LifeBuoy, LogOut, Settings, User } from "lucide-react";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -10,11 +10,16 @@ import {
 } from "../ui/dropdown-menu";
 import { Button } from "../ui/button";
 import { ModeToggle } from "./theme-mode-toggle";
+import { useNavigate } from "react-router-dom";
 
 export default function Header() {
+  const navigate = useNavigate();
+
   const logout = () => {
     localStorage.clear();
+    navigate("/login");
   };
+  const userName = localStorage.getItem("user").charAt(0).toLocaleUpperCase();
   return (
     <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
       <MobileSidebar />
@@ -22,19 +27,34 @@ export default function Header() {
       <ModeToggle />
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
-          <Button variant="secondary" size="icon" className="rounded-full">
-            <CircleUser className="h-5 w-5" />
+          <Button
+            variant="outline"
+            size="icon"
+            className="rounded-full border-2 border-black dark:border-white"
+          >
+            {userName}
             <span className="sr-only">Toggle user menu</span>
           </Button>
         </DropdownMenuTrigger>
         <DropdownMenuContent align="end">
           <DropdownMenuLabel>My Account</DropdownMenuLabel>
           <DropdownMenuSeparator />
-          <DropdownMenuItem>Settings</DropdownMenuItem>
-          <DropdownMenuItem>Switch Account</DropdownMenuItem>
+          <DropdownMenuItem>
+            <User className="mr-2 h-4 w-4" />
+            <span>Profile</span>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Settings className="mr-2 h-4 w-4" />
+            <span>Settings</span>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <LifeBuoy className="mr-2 h-4 w-4" />
+            <span>Support</span>
+          </DropdownMenuItem>
           <DropdownMenuSeparator />
           <DropdownMenuItem className="hover:cursor-pointer" onClick={logout}>
-            Logout
+            <LogOut className="mr-2 h-4 w-4" />
+            <span>Logout</span>
           </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>

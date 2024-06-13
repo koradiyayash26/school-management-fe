673ca595e71b735ec91ae469f8a4f77b6cf6e546
koradiyayash26 <yashkoradiya26@gmail.com>
2024-06-13 21:23:36 +0530

    animate sidebar , other changes

--- src/components/layouts/sidebar.jsx
+++ src/components/layouts/sidebar.jsx
@@ -1,48 +1,73 @@
-import { MENU } from "@/constant";
-import { Bell, ChevronLeft, ChevronRight, Package2 } from "lucide-react";
+import { MENU } from "@/constant"; // Adjust the path as needed
+import { ChevronLeft, ChevronRight, Package2 } from "lucide-react";
 import { NavLink } from "react-router-dom";
-import { Button } from "../ui/button";
 import Logout from "./logout";
+import PropTypes from "prop-types";
 
-function Sidebar() {
+function Sidebar({ collapsed, onToggle }) {
   return (
-    <div className="hidden border-r bg-muted/40 md:block">
+    <div
+      className={`hidden md:block border-r bg-muted/40 ${
+        collapsed
+          ? "w-[80px] transition-all duration-300 ease-in-out"
+          : "w-[220px] lg:w-[280px] transition-all duration-300 ease-in-out"
+      }`}
+    >
       <div className="flex h-full max-h-screen flex-col gap-2 overflow-auto sticky top-0">
-        <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6 shrink-0">
+        <div
+          className={`flex h-14 ${
+            collapsed && "justify-center"
+          } items-center border-b px-4 lg:h-[60px] lg:px-6 shrink-0`}
+        >
           <NavLink to="/" className="flex items-center gap-2 font-semibold">
             <Package2 className="h-6 w-6" />
-            <span className="">Thinker PVT LTD</span>
+            {!collapsed && <span>Thinker PVT LTD</span>}
           </NavLink>
         </div>
         <div className="flex-1 mt-[10px]">
-          <nav className="grid gap-2 items-start px-2 text-sm  font-medium lg:px-4">
+          <nav
+            className={`grid gap-2 ${
+              collapsed && "justify-center"
+            } items-start px-2 text-sm font-medium lg:px-4`}
+          >
             {MENU.map((item, index) => (
               <NavLink
                 key={index}
                 to={item.path}
                 className={({ isActive }) =>
                   isActive
-                    ? "flex items-center gap-3 rounded-lg px-3 py-2 bg-muted text-foreground transition-all hover:text-primary"
-                    : "flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+                    ? `${
+                        !collapsed && "flex items-center gap-3"
+                      } rounded-lg px-3 py-2 bg-muted text-foreground transition-all hover:text-primary`
+                    : `flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary ${
+                        !collapsed && "flex"
+                      }`
                 }
               >
                 {item.icon()}
-                {item.lable}
+                {!collapsed && item.lable}
               </NavLink>
             ))}
           </nav>
         </div>
-        <div className="mt-auto p-4">
-          <Logout />
+        <div className="mt-auto md:pl-2.5 lg:p-4">
+          <Logout collapsed={collapsed} />
         </div>
-        {/* sidebar arrow */}
         <div className="absolute top-1/2 right-0 transform -translate-y-1/2 hover:bg-muted cursor-pointer rounded-l-lg">
-          <ChevronLeft className="h-6 w-6" />
+          {collapsed ? (
+            <ChevronRight onClick={onToggle} className="h-6 w-6" />
+          ) : (
+            <ChevronLeft onClick={onToggle} className="h-6 w-6" />
+          )}
         </div>
-        {/* sidebar arrow */}
       </div>
     </div>
   );
 }
+
+Sidebar.propTypes = {
+  collapsed: PropTypes.bool.isRequired,
+  onToggle: PropTypes.func.isRequired,
+};
 
 export default Sidebar;

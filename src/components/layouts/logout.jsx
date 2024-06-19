6ae97428ad46f12fdf1f6605e38a862255ef9e19
koradiyayash26 <yashkoradiya26@gmail.com>
2024-06-19 18:29:24 +0530

    css add on logout for responsive

--- src/components/layouts/logout.jsx
+++ src/components/layouts/logout.jsx
@@ -13,7 +13,7 @@ function Logout({ collapsed }) {
         onClick={logout}
         className={`flex cursor-pointer md:px-3 ${
           collapsed && "justify-center"
-        } items-center gap-3 rounded-lg px-auto py-2 text-foreground transition-all hover:text-primary`}
+        } items-center gap-3 rounded-lg px-1 md:px-auto py-2 text-foreground transition-all hover:text-primary`}
       >
         <LogOut className="h-4 w-4" />
         {!collapsed && "Logout"}
